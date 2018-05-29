b9c91da8082aec9bbbdd2990876ed53fcf9baafc
Ganesh <id.ganesh123@gmail.com>
2018-05-29 16:45:36 +0530

    work

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var app = express();
-const data = require('./country.json')
+const country = require('./country.json')
+const city = require('./city.json')
 const port = process.env.PORT || 8080
 app.get('/country',(req,res)=>{
   res.header("Content-Type",'application/json');
   res.send(JSON.stringify(data));
 });
 app.get('/country',(req, res)=>{
-  res.json(data);
+  res.json(country);
+})
+app.get('/city',(req,res)=>{
+  res.header("Content-Type",'application/json');
+  res.send(JSON.stringify(data));
+});
+app.get('/city',(req, res)=>{
+  res.json(city);
 })
 app.listen(port,()=>{
   console.log("Server Established!");
