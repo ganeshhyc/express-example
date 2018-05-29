99aedac3980e51b1cd98ea5fce790ec4848bf040
Ganesh <id.ganesh123@gmail.com>
2018-05-29 18:21:04 +0530

    work

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ var express = require('express');
 var app = express();
 const country = require('./country.json');
 const city = require('./city.json');
+const statecity = require('./statecity.json');
 const port = process.env.PORT || 8080
 app.use(express.static(__dirname))
 app.get('/country',(req,res)=>{
   res.header("Content-Type",'application/json');
   res.send(JSON.stringify(country));
+});
+app.get('/statecity',(req, res)=>{
+  res.json(statecity);
+});
+app.get('/statecity',(req,res)=>{
+  res.header("Content-Type",'application/json');
+  res.send(JSON.stringify(statecity));
 });
 app.get('/country',(req, res)=>{
   res.json(country);
